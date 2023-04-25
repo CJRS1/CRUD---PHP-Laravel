12b3112b291736b130c45407abf0138cdd034a95
CJRS1 <reyes.christian@pucp.pe>
2023-04-25 13:20:57 -0700

    Se crearon los archivos para el Modelo, el Controlador y la migración

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Se usar resource para generar las rutas  */
+Route::resource('/products',ProductController::class);
